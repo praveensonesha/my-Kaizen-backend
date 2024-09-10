dbe2d52919d1faaa74f4004c0335d95b9cef9dac
psonesha <praveensonesha2003@gmail.com>
2024-09-10 10:50:56 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify, request
+import mysql.connector
+import os
+
+app = Flask(__name__)
+
+# MySQL Connection Function
+def get_mysql_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root123",
+        database="fitbot",
+        port=3306
+    )
+
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
+@app.route('/api/getReports', methods=['POST'])
+def api_data():
+    data = request.get_json()
+    if not data or 'userId' not in data:
+        return jsonify({"error": "No JSON data provided or userId missing"}), 400
+
+    user_id = data['userId']
+
+    print("user_id : ",user_id)
+    try:
+        connection = get_mysql_connection()
+        cursor = connection.cursor(dictionary=True)  # Use dictionary cursor to get results as dict
+        query = "SELECT reportDetails FROM reports WHERE UserId = %s"
+        cursor.execute(query, (user_id,))
+        records = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        if not records:
+            return jsonify({"message": "No records found for the given userId"}), 404
+        print("api response is : ",records)
+
+        return jsonify(records)
+    except mysql.connector.Error as err:
+        return jsonify({"error": str(err)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
