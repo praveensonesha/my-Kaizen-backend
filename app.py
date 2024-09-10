e218d858193a75aad23c8bd8934ebe8554ce6fd2
psonesha <praveensonesha2003@gmail.com>
2024-09-10 16:18:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,5 @@ def api_data():
         return jsonify({"error": str(err)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))  # Default to port 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port, debug=True)
